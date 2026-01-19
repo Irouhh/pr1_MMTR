c5b56711e3194d2ec2fe276f60d9bbc6ed955868
Irouhh <ikrbanka@gmail.com>
2026-01-19 16:39:06 +0300

    practice2final

---
+++ boards/script.js
@@ -0,0 +1,9 @@
+function openNewBoard() {
+    document.getElementById('createBoard').style.display = 'block';
+    document.getElementById('newBoard').style.display = 'none';
+}
+
+function cancelNewBoard() {
+    document.getElementById('createBoard').style.display = 'none';
+    document.getElementById('newBoard').style.display = 'block';
+}
---
+++ my_board/script.js
@@ -0,0 +1,9 @@
+function openNewList() {
+    document.getElementById('createList').style.display = 'block';
+    document.getElementById('newList').style.display = 'none';
+}
+
+function cancelNewList() {
+    document.getElementById('createList').style.display = 'none';
+    document.getElementById('newList').style.display = 'block';
+}
